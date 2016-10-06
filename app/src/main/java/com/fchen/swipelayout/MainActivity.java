d4029a556ab3bf9935b6ab363fd2ed983d0b29f0
Feng CHEN <fchensecurity@163.com>
2016-10-06 22:28:18 +0800

    Add actionbar and floatactionbar

--- app/src/main/java/com/fchen/swipelayout/MainActivity.java
+++ app/src/main/java/com/fchen/swipelayout/MainActivity.java
@@ -1,6 +1,9 @@
 package com.fchen.swipelayout;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.support.design.widget.FloatingActionButton;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -21,6 +24,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set up toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Snackbar action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
 
         // Get swipeLayout object
         swipeLayout = (SwipeLayout) findViewById(R.id.swipe_layout);
